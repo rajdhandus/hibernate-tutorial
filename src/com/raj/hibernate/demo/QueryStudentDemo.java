114478fd6c6f5015c55e1f3b9d2fcf73f37ac6b1
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-05 13:22:06 -0400

    Query DB using hibernate

---
+++ src/com/raj/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,47 @@
+package com.raj.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.raj.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure()
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			session.beginTransaction();
+			
+			List<Student> student = session.createQuery("from Student").getResultList();
+			
+			displayStudents(student);
+			
+			student = session.createQuery("from Student s where s.lastName='P'").getResultList();
+			
+			displayStudents(student);
+			
+			session.getTransaction().commit();
+		}
+		finally{
+			factory.close();
+		}
+
+	}
+
+	private static void displayStudents(List<Student> student) {
+		for(Student tmpStudent:student){
+			System.out.println(tmpStudent.toString());
+		}
+	}
+
+}
