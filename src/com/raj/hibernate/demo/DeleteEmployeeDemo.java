f74bb28050690869ff156f97a293aee0c0a86e4e
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-05 20:13:03 -0400

    Delete Employee from DataBase using Hibernate

--- src/com/raj/hibernate/demo/DeleteEmployeeDemo.java
+++ src/com/raj/hibernate/demo/DeleteEmployeeDemo.java
@@ -1,10 +1,34 @@
 package com.raj.hibernate.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.raj.hibernate.demo.entity.Employee;
+
 public class DeleteEmployeeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		try{
+			int empId = 1;
+			
+			session.beginTransaction();
+			
+			Employee tempEmployee = session.get(Employee.class, empId);
+			
+			session.delete(tempEmployee);
+			
+			session.createQuery("delete Employee e where id='2'").executeUpdate();
+			
+			session.getTransaction().commit();
+		}
+		finally{
+			factory.close();
+		}
+		
 	}
 
 }
